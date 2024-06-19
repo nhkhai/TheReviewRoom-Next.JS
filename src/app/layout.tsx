eea25c38aaf16cefaecde660a583b66cae68c562
Ng Heng Khai <nhkhai@gmail.com>
2024-06-19 15:07:49 +0800

    Added the Material UI integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+// import { CssVarsProvider } from '@mui/material/styles'; // If using CSS theme variables.
+import theme from "../theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <AppRouterCacheProvider options={{ key: 'css' }}> // If overriding the default cache options to use a custom cache instead. To change the CSS key to css (the default is mui).  */}
+        {/* <AppRouterCacheProvider options={{ enableCssLayer: true }}> // If using a styling solution other than Emotion to customize Material UI components.  */}
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
